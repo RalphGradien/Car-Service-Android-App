364acf8deec2973045074ee8fecc8cb4768b0645
davinci8gilbert <105763346+davinci8gilbert@users.noreply.github.com>
2023-03-07 11:49:23 -0800

    gilbert_customerViewAppointments_uptoCustomerAppointmentAdapterBinding

--- app/src/main/java/com/example/carserviceandroidapp/CustomerAppointmentAdapter.java
+++ app/src/main/java/com/example/carserviceandroidapp/CustomerAppointmentAdapter.java
@@ -1,24 +1,42 @@
 package com.example.carserviceandroidapp;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.List;
+
 public class CustomerAppointmentAdapter extends RecyclerView.Adapter<CustomerAppointmentViewHolder> {
+    Context context;
+    List<CustomerApointmentItems> customerApointmentItemsList;
+
+    public CustomerAppointmentAdapter(Context context, List<CustomerApointmentItems> customerApointmentItemsList) {
+        this.context = context;
+        this.customerApointmentItemsList = customerApointmentItemsList;
+    }
+
     @NonNull
     @Override
     public CustomerAppointmentViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        return new CustomerAppointmentViewHolder(LayoutInflater.from(context).inflate(R.layout.customer_appointmentsview_item,parent,false));
     }
 
     @Override
     public void onBindViewHolder(@NonNull CustomerAppointmentViewHolder holder, int position) {
-
+        holder.appointmentIDTextView.setText(customerApointmentItemsList.get(position).getAppointmentIDText());
+        holder.appointmentDateView.setText(customerApointmentItemsList.get(position).getAppointmentDate());
+        holder.appointmentTimeView.setText(customerApointmentItemsList.get(position).getAppointmentTime());
+        holder.bookedServiceProviderNameView.setText(customerApointmentItemsList.get(position).getBookedServiceProviderName());
+        holder.bookedServiceProviderAddressView.setText(customerApointmentItemsList.get(position).getBookedServiceProviderAddress());
+        holder.serviceAvailedView.setText(customerApointmentItemsList.get(position).getServiceAvailed());
+        holder.bookingStatusView.setText(customerApointmentItemsList.get(position).getBookingStatus());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return customerApointmentItemsList.size();
     }
 }
