c0839c5eb103246159d11f167affc822a98e3596
davinci8gilbert <105763346+davinci8gilbert@users.noreply.github.com>
2023-03-07 12:02:56 -0800

    gilbert_customerViewAppointments_uptoCustomerAppointmentsViewDataPopulate

--- app/src/main/java/com/example/carserviceandroidapp/Customer_AppointmentsView.java
+++ app/src/main/java/com/example/carserviceandroidapp/Customer_AppointmentsView.java
@@ -1,8 +1,13 @@
 package com.example.carserviceandroidapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Customer_AppointmentsView extends AppCompatActivity {
 
@@ -10,5 +15,17 @@ public class Customer_AppointmentsView extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer_appointments_view);
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerviewCustViewAppointments);
+        List<CustomerApointmentItems> customerApointmentItemsList = new ArrayList<CustomerApointmentItems>();
+        customerApointmentItemsList.add(new CustomerApointmentItems(12345,"2022-10-12","09:40PM",
+                "","CarServiceProvider1","New Westminster","Brake Check",
+                "Completed"));
+        customerApointmentItemsList.add(new CustomerApointmentItems(12346,"2022-11-8","07:30AM",
+                "","CarServiceProvider2","Abbotsford","Steering Wheel Check",
+                "Ongoing"));
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(new CustomerAppointmentAdapter(getApplicationContext(),customerApointmentItemsList));
     }
 }
