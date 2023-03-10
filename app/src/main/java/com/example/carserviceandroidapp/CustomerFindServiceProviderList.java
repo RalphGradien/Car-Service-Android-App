56ab19c6d66835e897b1837b4401d4e2321b8eeb
arifinw <arifinw@gmail.com>
2023-03-10 10:43:15 -0800

    Arifin 10 March 2023

---
+++ app/src/main/java/com/example/carserviceandroidapp/CustomerFindServiceProviderList.java
@@ -0,0 +1,14 @@
+package com.example.carserviceandroidapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CustomerFindServiceProviderList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_find_service_provider_list);
+    }
+}
