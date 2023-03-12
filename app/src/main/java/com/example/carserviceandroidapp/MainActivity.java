31d39545d96911f3e0b892841fe52c70495f09ca
kenleano <kenleano@gmail.com>
2023-03-11 20:29:36 -0800

    Added login functionality with stubs

--- app/src/main/java/com/example/carserviceandroidapp/MainActivity.java
+++ app/src/main/java/com/example/carserviceandroidapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.carserviceandroidapp;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
--- homeLogin/src/main/java/com/example/gark/LogIn.java
+++ homeLogin/src/main/java/com/example/gark/LogIn.java
@@ -5,7 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class LogIn extends AppCompatActivity {
 
@@ -13,7 +16,27 @@ public class LogIn extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+        EditText username = findViewById(R.id.usernameTxt);
+        EditText password = findViewById(R.id.passwordTxt);
 
+        Button login = findViewById(R.id.loginBtn2);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String enteredUsername = username.getText().toString();
+                String enteredPassword = password.getText().toString();
+                DBHelper dbHelper = new DBHelper(LogIn.this);
+                boolean loginSuccessful = dbHelper.checkLogin(enteredUsername, enteredPassword);
+                if (loginSuccessful) {
+                    // login successful, do something
+                    Intent intent = new Intent(LogIn.this, home.class);
+                    startActivity(intent);
+                } else {
+                    // login failed, show error message
+                    Toast.makeText(getApplicationContext(), "Incorrect username or password", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
         TextView regServiceTxt = findViewById(R.id.regService);
         regServiceTxt.setOnClickListener(new View.OnClickListener() {
             @Override
