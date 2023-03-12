dbe3b8b3635e466f124274f48d5fc9544e1526e1
kenleano <kenleano@gmail.com>
2023-03-11 20:34:06 -0800

    Added login functionality with stubs

--- homeLogin/src/main/java/com/example/gark/registerService.java
+++ homeLogin/src/main/java/com/example/gark/registerService.java
@@ -9,6 +9,6 @@ public class registerService extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register_service2);
+        setContentView(R.layout.activity_register_service);
     }
 }
