0d9e6d2cc6c63d263b7dd138c3d8ac54db0ff6ee
arifinw <arifinw@gmail.com>
2023-03-14 00:36:21 -0700

    Ari add activities and xml

---
+++ app/src/main/java/com/example/carserviceandroidapp/CustomerFindServiceProviderList.java
@@ -0,0 +1,14 @@
+package com.example.carserviceandroidapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CustomerFindServiceProviderList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_find_service_provider_list);
+    }
+}
