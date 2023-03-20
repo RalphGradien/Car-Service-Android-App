17db828139a075589c3290695e0c534348e13218
kenleano <kenleano@gmail.com>
2023-03-19 20:33:04 -0700

    Added Name change to Service Profile

--- app/src/main/java/com/example/carserviceandroidapp/DBHelper.java
+++ app/src/main/java/com/example/carserviceandroidapp/DBHelper.java
@@ -410,8 +410,13 @@ public class DBHelper extends SQLiteOpenHelper {
             // Failed to update record
             return false;
         }
-
     }
+    public Cursor getServiceProviderName(int ID) {
+        SQLiteDatabase DB = this.getWritableDatabase();
+        Cursor cursor = DB.rawQuery("SELECT serviceProviderFullName FROM SERVICE_PROVIDER WHERE ServiceProviderID = ?", new String[] { String.valueOf(ID) });
+        return cursor;
+    }
+
 }
 
 
