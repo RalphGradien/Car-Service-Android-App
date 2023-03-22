53d026fcd798f67e5e2f6802a37c0ed6140d4d55
davinci8gilbert <105763346+davinci8gilbert@users.noreply.github.com>
2023-03-22 00:22:33 -0700

    updated_todisplay image

--- app/src/main/java/com/example/carserviceandroidapp/ServiceAccount.java
+++ app/src/main/java/com/example/carserviceandroidapp/ServiceAccount.java
@@ -7,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -22,14 +23,59 @@ public class ServiceAccount extends Fragment {
         Button editProfile = rootView.findViewById(R.id.editProfileBtn);
         TextView profileName = rootView.findViewById(R.id.profileName);
 
+
+        ImageView imageProfile = rootView.findViewById(R.id.imageView6);
+
         //Retrieves Service Provider Name from DBHelper
         DBHelper db = new DBHelper(getActivity());
         int spID = ServiceProvider.ServiceProviderID;
+        int cID = Customer.CustomerID;
         Cursor cursor = db.getServiceProviderName(spID);
-        if (cursor.moveToFirst()) {
-            String spName = cursor.getString(cursor.getColumnIndexOrThrow("serviceProviderFullName"));
-            profileName.setText(spName);
+        cursor.moveToFirst();
+        Cursor custCursor = db.getCustomerName(cID);
+        custCursor.moveToFirst();
+
+        Cursor imageCursor = db.getServiceProviderImage(spID);
+        imageCursor.moveToFirst();
+
+        String imageLetter=imageCursor.getString(0);
+
+
+        int image = 0;
+        if(imageLetter .equals("a")){
+            image = com.example.gark.R.drawable.a;
         }
+        if(imageLetter .equals("b")){
+            image = com.example.gark.R.drawable.b;
+        }
+        if(imageLetter .equals("c")){
+            image = com.example.gark.R.drawable.c;
+        }
+        if(imageLetter .equals("d")){
+            image = com.example.gark.R.drawable.d;
+        }
+        if(imageLetter .equals("e")){
+            image = com.example.gark.R.drawable.e;
+        }
+
+        if(imageLetter .equals("f")){
+            image = com.example.gark.R.drawable.f;
+        }
+        if(imageLetter .equals("g")){
+            image = com.example.gark.R.drawable.g;
+        }
+        if(imageLetter .equals("h")){
+            image = com.example.gark.R.drawable.h;
+        }
+
+        if(spID>0){
+            profileName.setText(cursor.getString(0));
+            imageProfile.setImageResource(image);
+        }
+        else if (cID>0){
+            profileName.setText(custCursor.getString(0));
+        }
+
 
         Button logOut = rootView.findViewById(R.id.logOut);
 
